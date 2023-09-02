288434b01276ba6c111ebb05d40e685bfebeb4b8
maddc0de <maddc0de@outlook.com>
2023-09-02 22:48:59 +0100

    use namedtuples to process data from the csv file

--- practice_3/reading_and_writing_files.py
+++ practice_3/reading_and_writing_files.py
@@ -1,20 +1,15 @@
 import csv
+from collections import namedtuple
+
+Book = namedtuple("Book", "name author user_rating reviews price year genre")
 
 books = []
 
 with open("bestsellers_with_categories.csv", "r", encoding="utf-8") as csvfile:
     reader = csv.reader(csvfile, skipinitialspace=True)
     next(reader) # skips first row
-    for row in reader: # create dictionary from every row
-        new_book = {}
-        new_book["name"] = row[0]
-        new_book["author"] = row[1]
-        new_book["user_rating"] = row[2]
-        new_book["reviews"] = row[3]
-        new_book["price"] = row[4]
-        new_book["year"] = row[5]
-        new_book["genre"] = row[6]
-
+    for row in reader:
+        new_book = Book(row[0], row[1], row[2], row[3], row[4], row[5], row[6])
         books.append(new_book)
 
-print(books[0])
+print(books[0].name)
