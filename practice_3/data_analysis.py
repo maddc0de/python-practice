72dd84494c6c938f96697f001a78caa710b14ee0
maddc0de <maddc0de@outlook.com>
2023-09-05 13:08:56 +0100

    use panda to collect top ten authors with the most amount of bestselling books

--- practice_3/data_analysis.py
+++ practice_3/data_analysis.py
@@ -1,5 +1,15 @@
 import pandas as pd
 
 bestsellers = pd.read_csv("bestsellers_with_british_price.csv")
-
 print(bestsellers)
+# print(bestsellers.Name)
+
+# remove duplicates
+bestsellers = bestsellers.drop_duplicates(subset="Name", keep="last")
+print(bestsellers)
+
+# create bar chart showing the author with the most amount of bestselling titles in the given years
+  # authors, number of bestselling books
+number_of_bestsellers_written = bestsellers.groupby("Author")[["Name"]].count().sort_values("Name", ascending=False).head(10)
+
+print(number_of_bestsellers_written)
