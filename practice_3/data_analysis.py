f3d2ed33f22f949db8a1db55165482ef94400a8e
maddc0de <maddc0de@outlook.com>
2023-09-05 13:24:50 +0100

    use matplotlib to create a bar chart of top 10 authors with the most bestselling books

--- practice_3/data_analysis.py
+++ practice_3/data_analysis.py
@@ -1,7 +1,7 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 bestsellers = pd.read_csv("bestsellers_with_british_price.csv")
-print(bestsellers)
 # print(bestsellers.Name)
 
 # remove duplicates
@@ -9,7 +9,10 @@ bestsellers = bestsellers.drop_duplicates(subset="Name", keep="last")
 print(bestsellers)
 
 # create bar chart showing the author with the most amount of bestselling titles in the given years
-  # authors, number of bestselling books
-number_of_bestsellers_written = bestsellers.groupby("Author")[["Name"]].count().sort_values("Name", ascending=False).head(10)
-
+  # compare authors with how many number of bestselling books they have
+number_of_bestsellers_written = bestsellers.groupby("Author")[["Name"]].count().sort_values("Name", ascending=False).head(10).reset_index()
 print(number_of_bestsellers_written)
+
+  # create bar chart
+plt.bar(number_of_bestsellers_written.Author, number_of_bestsellers_written.Name, color="blue", width=0.4)
+plt.show()
